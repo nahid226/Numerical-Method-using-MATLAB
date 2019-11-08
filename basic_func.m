ea406b6afd2802e326481ae0acabeec0990ad756
Nahid Babu <45840380+nahid226@users.noreply.github.com>
2019-11-08 13:38:23 -0800

    basic function added

---
+++ basic_func.m
@@ -0,0 +1,16 @@
+clc 
+clear all
+format long
+z = 4 + 3 * i; % i or j implies imaginary number
+phase_angle = angle(z); %phase angle in radian
+
+angle_in_degree = angle(z) *(180/pi);
+
+imaginary = imag(z);
+
+real = real(z);
+complex_conjugate = conj(z);
+
+z1 = 4.9 * -1;
+abs(z1)
+
