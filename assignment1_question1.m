ad82b54cc29f042f6127a6795a63045ea1629403
Nahid Babu <45840380+nahid226@users.noreply.github.com>
2019-11-08 13:40:56 -0800

    assignment-01 added

---
+++ assignment1_question1.m
@@ -0,0 +1,9 @@
+clc
+
+% calculate : sin(225) + cot(30) + tan-1(30) + 10e-10 + 9x10-2 + (log1010)3 + (loge10)3
+% values in degree
+clear all
+format long
+
+
+c = sind(225) + cotd(30) + atand(30) + 10e-10 +9e-2 + log10(10)^3 + (log(10))^3
---
+++ assignment1_question2.m
@@ -0,0 +1,6 @@
+clc 
+clear all
+format long 
+% calculate : y = (e)^(5^2/3*pi)
+
+y = exp((5^2)/(3*pi))
