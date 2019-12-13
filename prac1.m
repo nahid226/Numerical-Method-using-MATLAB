a02f2c5ce693af6d4f89ff337084b3f79735195b
Nahid Babu <45840380+nahid226@users.noreply.github.com>
2019-12-14 00:56:02 +0600

    Add files via upload

---
+++ prac1.m
@@ -0,0 +1,24 @@
+clear all;
+clc;
+
+z = 4 + 3 * i;
+mag = abs(z);
+phase_angle = angle(z);
+
+phase_angle_in_degree = angle(z) * 180/pi;
+
+imaginary_part = imag(z);
+real_part = real(z);
+complex_conjugate  = conj(z);
+
+y = linspace(0,10,11);
+y;
+x = 10 * exp(-2);
+a = (log10(10))^4;
+b = (log2(8))^4;
+
+ans = sind(225) + cotd(30) + atand(30) + 9e-2 + (log10(10))^3 + (log(10))^3
+
+ans2 = exp((5^2)/(3*pi))
+
+
